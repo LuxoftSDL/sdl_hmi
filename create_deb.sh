6082ec1ed465bd3102d2769f5ccac9d4f3bfd50d
Alexander Kutsan <akutsan@luxoft.com>
2020-05-17 10:36:51 +0300

    Fix review comments

--- create_deb.sh
+++ create_deb.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 PROGRAM_NAME=sdlhmi
-VERSION=6.1.0
+VERSION=6.2.0
 PROGRAM_NAME=$PROGRAM_NAME-$VERSION
 
 rm -rf $PROGRAM_NAME
@@ -9,12 +9,15 @@ cp -r  deb/* $PROGRAM_NAME/
 mkdir -p $PROGRAM_NAME/usr/bin/
 mkdir -p $PROGRAM_NAME/usr/share/sdlhmi
 mkdir -p $PROGRAM_NAME/usr/share/doc/sdlhmi
-cp sdlhmi $PROGRAM_NAME/usr/bin/
-mv * $PROGRAM_NAME/usr/share/sdlhmi/; cp -r $PROGRAM_NAME/usr/share/sdlhmi/* .
+cp sdlhmi $PROGRAM_NAME/usr/bin
+
+for entity in $(ls);do
+  cp -rv $entity $PROGRAM_NAME/usr/share/sdlhmi/
+done
+
 gzip -9 --keep --stdout $PROGRAM_NAME/DEBIAN/changelog >  \
 	                 $PROGRAM_NAME/usr/share/doc/sdlhmi/changelog.gz
 cp $PROGRAM_NAME/DEBIAN/copyright $PROGRAM_NAME/usr/share/doc/sdlhmi/
-#(cd $PROGRAM_NAME; md5deep -r usr > DEBIAN/md5sums)
 fakeroot dpkg-deb --build $PROGRAM_NAME
 lintian $PROGRAM_NAME.deb
 
