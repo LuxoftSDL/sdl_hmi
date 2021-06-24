b1be6a7ad055bdf82346eb9038940437c7f1d351
valerii <valera.malkov@gmail.com>
2021-06-24 17:36:46 +0300

    Fix AAW-910

--- app/view/sdl/AlertManeuverPopUp.js
+++ app/view/sdl/AlertManeuverPopUp.js
@@ -236,7 +236,12 @@ SDL.AlertManeuverPopUp = Em.ContainerView.create(
       const resultCode = this.iconsAreValid ?
         SDL.SDLModel.data.resultCode.SUCCESS : SDL.SDLModel.data.resultCode.WARNINGS;
       
-
+      if(!message) {
+        if (SDL.ResetTimeoutPopUp.resetTimeoutRPCs.includes('TTS.Speak')) {
+          SDL.SDLController.TTSResponseHandler();
+          SDL.ResetTimeoutPopUp.resetTimeoutRPCs.removeObject('TTS.Speak');
+        } 
+      }
       FFW.Navigation.sendNavigationResult(
         resultCode,
         this.alertManeuerRequestId,
